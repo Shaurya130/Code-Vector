243819a3a74ed79f3cb41b89d9271a4bdf9e88c3
Shaurya Awasthi <shaurya.awasthi130@gmail.com>
2026-06-24 16:22:31 +0530

    added README

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import productRoutes from "./routes/products.js";
+import cors from "cors";
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (_req, res) => {
